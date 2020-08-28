2e98b76dd10e7cc0c656bfe775f958b8587a4dd1
Brian Li <antai0926@gmail.com>
2020-08-28 13:52:00 +0800

    modify for deploying netlify

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
-  <BrowserRouter basename="/love-animal-react">
+  <BrowserRouter>
     <React.StrictMode>
       <App />
     </React.StrictMode>
